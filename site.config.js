4094547fc69b81e7ef0c6c729c6982b226a688b9
ajrichter7 <48424312+ajrichter7@users.noreply.github.com>
2023-06-18 14:21:58 +0200

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -24,7 +24,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "morethan-qliwbnrl7-ajrichter7.vercel.app",
+  link: "morethan-log-ajrichter7.vercel.app",
   since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
